bf5857c67a9c063c96cd7fad4a60ae8c31b4a19c
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-11-30 21:06:12 +0000

    Testing multiple pages

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,10 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.title("Cup Adventure")
+from PIL import Image
+image = Image.open(r"images\banner.jpg")
+
+st.image(image)
 # st.image("images\logo.jpg")
 # st.image(r"images\banner.jpg")
 
@@ -10,7 +14,7 @@ st.title("Cup Adventure")
 # sourceCode = HTMLFile.read()
 # components.html(sourceCode)
 
-st.subheader("Website Code")
-HTMLFile = open('final_form_website.html', 'r', encoding='utf-8')
-sourceCode = HTMLFile.read()
-components.html(sourceCode)
+# st.subheader("Website Code")
+# HTMLFile = open('final_form_website.html', 'r', encoding='utf-8')
+# sourceCode = HTMLFile.read()
+# components.html(sourceCode)
