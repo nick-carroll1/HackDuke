73eace39750b27ac5ab90a1c71b7bcc2cd1eeb8e
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-11-30 20:57:24 +0000

    Testing html

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,12 @@ import streamlit.components.v1 as components
 
 st.title("Cup Adventure")
 # st.image("images\logo.jpg")
-st.image(r"images\banner.jpg")
+# st.image(r"images\banner.jpg")
 
-# st.subtitle("Previous Code")
-# HTMLFile = open('hack.html', 'r', encoding='utf-8')
-# sourceCode = HTMLFile.read()
-# components.html(sourceCode)
+st.subtitle("Previous Code")
+HTMLFile = open('hack.html', 'r', encoding='utf-8')
+sourceCode = HTMLFile.read()
+components.html(sourceCode)
 
 # st.subtitle("Website Code")
 # HTMLFile = open('final_form_website.html', 'r', encoding='utf-8')
