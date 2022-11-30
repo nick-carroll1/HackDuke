5318983db9efbb03eec67bc3f4f9fb2b487905b2
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-11-30 20:46:59 +0000

    First Try at Streamlit App

---
+++ streamlit_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.title("Cup Adventure")
+st.image("images\logo.jpg")
+st.image(r"images\banner.jpg")
+
+st.subtitle("Previous Code")
+HTMLFile = open('hack.html', 'r', encoding='utf-8')
+sourceCode = HTMLFile.read()
+components.html(sourceCode)
+
+st.subtitle("Website Code")
+HTMLFile = open('final_form_website.html', 'r', encoding='utf-8')
+sourceCode = HTMLFile.read()
+components.html(sourceCode)
