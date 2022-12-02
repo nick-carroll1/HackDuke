0e70afa989364f81aa49e666ed5498d5d4fe0be3
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-12-02 16:37:46 +0000

    Still dealing with dependencies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,29 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import streamlit_authenticator as stauth
+# import streamlit_authenticator as stauth
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
-)
+# authenticator = stauth.Authenticate(
+#     config['credentials'],
+#     config['cookie']['name'],
+#     config['cookie']['key'],
+#     config['cookie']['expiry_days'],
+#     config['preauthorized']
+# )
 
-with open('../config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
+# with open('../config.yaml') as file:
+#     config = yaml.load(file, Loader=SafeLoader)
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+# name, authentication_status, username = authenticator.login('Login', 'main')
 
-if authentication_status:
-    authenticator.logout('Logout', 'main')
-    st.title("Cup Adventure")
-    st.write(f'Welcome *{name}*')
-    st.write(config['credentials'])
-elif authentication_status == False:
-    st.error('Username/password is incorrect')
-elif authentication_status == None:
-    st.warning('Please enter your username and password')
+# if authentication_status:
+#     authenticator.logout('Logout', 'main')
+#     st.title("Cup Adventure")
+#     st.write(f'Welcome *{name}*')
+#     st.write(config['credentials'])
+# elif authentication_status == False:
+#     st.error('Username/password is incorrect')
+# elif authentication_status == None:
+#     st.warning('Please enter your username and password')
 
 # from PIL import Image
 # image = Image.open(r"images\banner.jpg")
