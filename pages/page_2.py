db24fbe4ced11f3806a51101fb9a761417039ebe
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-12-02 03:29:39 +0000

    Updating new user functionality

--- pages/page_2.py
+++ pages/page_2.py
@@ -8,5 +8,7 @@ st.subheader("Welcome to Cup Adventure!")
 
 first_name = st.text_input("Add your first name")
 last_name = st.text_input("Add your last name")
-user = {"customer_firstName": first_name, "customer_lastName": last_name, "join_date": date.today().__str__()}
-createdb.add_user(user)
+st.write(first_name)
+if (first_name != None) and (last_name != None):
+    user = {"customer_firstName": first_name, "customer_lastName": last_name, "join_date": date.today().__str__()}
+    createdb.add_user(user)
