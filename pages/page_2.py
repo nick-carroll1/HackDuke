652ec2a096b9de364f8b6f6de286bac207491c74
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-12-02 03:38:08 +0000

    Updating customer sign-up functionality

--- pages/page_2.py
+++ pages/page_2.py
@@ -11,4 +11,9 @@ last_name = st.text_input("Add your last name")
 st.write(first_name)
 if (first_name != "") and (last_name != ""):
     user = {"customer_firstName": first_name, "customer_lastName": last_name, "join_date": date.today().__str__()}
-    createdb.add_user(user)
+    try:
+        createdb.add_user(user)
+        st.write(f"Congratulations {first_name} {last_name}!  You have signed-up for Cup Adventure!")
+        st.write("Thank you for joining us in reducing Cup Waste!")
+    except:
+        st.write("There was an error signing you up.  Please ensure both your first name and last name are not blank.")
