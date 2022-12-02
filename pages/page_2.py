f4beee5752503234f02b3ce5db0cf27dd7baa3a4
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-12-02 03:31:50 +0000

    Fixing blank name functionality

--- pages/page_2.py
+++ pages/page_2.py
@@ -9,6 +9,6 @@ st.subheader("Welcome to Cup Adventure!")
 first_name = st.text_input("Add your first name")
 last_name = st.text_input("Add your last name")
 st.write(first_name)
-if (first_name != None) and (last_name != None):
+if (first_name != "") and (last_name != ""):
     user = {"customer_firstName": first_name, "customer_lastName": last_name, "join_date": date.today().__str__()}
     createdb.add_user(user)
