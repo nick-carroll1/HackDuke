81f1340ea3ccefc7cc805493355c212a530e3ae9
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-12-13 18:39:46 +0000

    removing warning suppression

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ def check_password():
         # Password correct.
         return True
 
-@st.cache(suppress_st_warning=True)
+@st.cache
 def main():
     st.title("Cup Adventure")
     st.write(st.session_state)
