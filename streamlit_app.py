f259459ebeceec46fe983fc6b3d8964a62ae6992
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-12-13 18:58:59 +0000

    Troubleshooting session state

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,35 +54,36 @@ def main():
             results = query(userquery)
             userInfo = {'firstName': results[0][0], 'username': results[0][1], 'status': results[0][2]}
             st.session_state['user info'] = userInfo
-        st.write(f"Welcome {st.session_state['user info']['firstName']}")
-        if userInfo['status'] == "Borrowed":
-            st.write("You currently have a cup borrowed.  Please return your cup when you are finished with it.")
-        elif userInfo['status'] == "Available":
-            st.write("If you would like to rent a cup, please use the dropdown below.")
-            with st.form("rental"):
-                # First run, show inputs for username + password.
-                st.selectbox("Please select a vendor", ["Starbucks", "Beyu"])
-                st.selectbox("Please select a cup", ["Cup 1", "Cup 2"])
-                # Every form must have a submit button.
-                submitted = st.form_submit_button("Submit")
-                st.write(submitted)
-                # if submitted:
-                #     # cup_rental()
-                #     st.write("Thank you for renting your cup.")
-        elif userInfo['status'] == None:
-            st.write("Use the dropdown below to rent your first cup.")
-            with st.form("first_rental"):
-                # First run, show inputs for username + password.
-                st.selectbox("Please select a vendor", ["Starbucks", "Beyu"])
-                st.selectbox("Please select a cup", ["Cup 1", "Cup 2"])
-                # Every form must have a submit button.
-                submitted = st.form_submit_button("Submit")
-                # st.write(submitted)
-                # if submitted:
-                #     # cup_rental()
-                #     st.write("Thank you for renting your cup.")
         else:
-            st.write("There has been an error tracking your last cup.  Please contact us for help.")
+            st.write(f"Welcome {st.session_state['user info']['firstName']}")
+            if st.session_state['user info']['status'] == "Borrowed":
+                st.write("You currently have a cup borrowed.  Please return your cup when you are finished with it.")
+            elif st.session_state['user info']['status'] == "Available":
+                st.write("If you would like to rent a cup, please use the dropdown below.")
+                with st.form("rental"):
+                    # First run, show inputs for username + password.
+                    st.selectbox("Please select a vendor", ["Starbucks", "Beyu"])
+                    st.selectbox("Please select a cup", ["Cup 1", "Cup 2"])
+                    # Every form must have a submit button.
+                    submitted = st.form_submit_button("Submit")
+                    st.write(submitted)
+                    # if submitted:
+                    #     # cup_rental()
+                    #     st.write("Thank you for renting your cup.")
+            elif st.session_state['user info']['status'] == None:
+                st.write("Use the dropdown below to rent your first cup.")
+                with st.form("first_rental"):
+                    # First run, show inputs for username + password.
+                    st.selectbox("Please select a vendor", ["Starbucks", "Beyu"])
+                    st.selectbox("Please select a cup", ["Cup 1", "Cup 2"])
+                    # Every form must have a submit button.
+                    submitted = st.form_submit_button("Submit")
+                    # st.write(submitted)
+                    # if submitted:
+                    #     # cup_rental()
+                    #     st.write("Thank you for renting your cup.")
+            else:
+                st.write("There has been an error tracking your last cup.  Please contact us for help.")
 
 def cup_rental():
     st.write("Thank you for renting your cup.")
