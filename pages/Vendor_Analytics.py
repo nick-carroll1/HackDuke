ec7036cdf04d97190daa85d17ae3c3ae18abf377
alisa0705 <89174034+alisa0705@users.noreply.github.com>
2022-12-14 22:57:30 -0800

    Update Vendor_Analytics.py

--- pages/Vendor_Analytics.py
+++ pages/Vendor_Analytics.py
@@ -3,10 +3,15 @@ import pandas as pd
 import numpy as np
 import altair
 
-df=pd.read_excel("https://github.com/nick-carroll1/HackDuke/blob/main/raw_data/transactions_log.xlsx?raw=true")
-df.head()
-
 st.title("Vendor Analytics")
 
 
 st.title("unique users")
+
+df=pd.read_excel("https://github.com/nick-carroll1/HackDuke/blob/main/raw_data/transactions_log.xlsx?raw=true")
+d=df.groupby('transaction_date')['cup_id'].nunique()
+d1 = d.to_frame().reset_index()
+
+altair.Chart(d1).mark_line().encode(x='transaction_date',y='cup_id')
+
+
