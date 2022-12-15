4f2d5f86ba0b9115b4afa83bc75eb0fddbc656c5
alisa0705 <89174034+alisa0705@users.noreply.github.com>
2022-12-14 22:55:49 -0800

    Update Vendor_Analytics.py

--- pages/Vendor_Analytics.py
+++ pages/Vendor_Analytics.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+df=pd.read_excel("https://github.com/nick-carroll1/HackDuke/blob/main/raw_data/transactions_log.xlsx?raw=true")
+df.head()
+
 st.title("Vendor Analytics")
 
 
