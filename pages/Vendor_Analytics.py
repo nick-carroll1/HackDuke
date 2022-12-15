cba2f7c02003f012635ea611888bc9f7976c2496
alisa0705 <89174034+alisa0705@users.noreply.github.com>
2022-12-14 22:56:41 -0800

    Update Vendor_Analytics.py

--- pages/Vendor_Analytics.py
+++ pages/Vendor_Analytics.py
@@ -1,4 +1,7 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import altair
 
 df=pd.read_excel("https://github.com/nick-carroll1/HackDuke/blob/main/raw_data/transactions_log.xlsx?raw=true")
 df.head()
