ba5995ff9abd8a3bc24e186cbdd4e39e82b8c279
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-12-15 23:07:17 +0000

    Troubleshooting scanner

--- pages/Scan.py
+++ pages/Scan.py
@@ -5,12 +5,12 @@ import os
 components.html(
     """<?php SESSION_START(); ?>
 <html>
-    <head>  
-    <script type = "text/javascript" src = "https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
-    <script type = "text/javascript" src = "https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
-    <script type = "text/javascript" src = "https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
-    <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    </head>
+    # <head>  
+    # <script type = "text/javascript" src = "https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
+    # <script type = "text/javascript" src = "https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
+    # <script type = "text/javascript" src = "https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+    # <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    # </head>
     <body>
         <div class = "container">
             <div class = "row">
