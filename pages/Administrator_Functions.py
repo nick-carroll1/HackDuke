28e6c7f8e158b9bffeda8c04a5ad96394310bd55
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-12-15 21:52:47 +0000

    Getting rid of match case

--- pages/Administrator_Functions.py
+++ pages/Administrator_Functions.py
@@ -160,206 +160,194 @@ def check_password():
 
 
 st.title("Cup Adventure")
-
-import sys
-st.write(sys.version)
-
-# match selection:
-#     case "Welcome Page":
-#         st.write(selection)
-#     case "Read All Data":
-#         st.write(selection)
-
-
 # if check_password():
-# match selection:
-#     # if the user selects "Read Data" then show the table
-#     case "Welcome Page":
-#         st.write("Welcome to Cup Adventure Admin Page")
-#         st.write("Please select a page from the sidebar")
+# if the user selects "Read Data" then show the table
+if selection == "Welcome Page":
+    st.write("Welcome to Cup Adventure Admin Page")
+    st.write("Please select a page from the sidebar")
 
-#     case "Read All Data":
-#         # create a streamlit selectbox to select the table
-#         table_name = st.selectbox("Select a table", tables)
+elif selection == "Read All Data":
+    # create a streamlit selectbox to select the table
+    table_name = st.selectbox("Select a table", tables)
 
-#         # query the database based on table_name selected and preserve the column names
-#         query = "SELECT * FROM " + table_name
-#         df = pd.read_sql(query, connection)
-#         st.write(df)
+    # query the database based on table_name selected and preserve the column names
+    query = "SELECT * FROM " + table_name
+    df = pd.read_sql(query, connection)
+    st.write(df)
 
-#         # create a streamlit selectbox to select the column
-#         column_name = st.selectbox("Select a column", df.columns)
+    # create a streamlit selectbox to select the column
+    column_name = st.selectbox("Select a column", df.columns)
 
-#         # make a summary statistics table of the selected column in horizontal format and integer format
-#         st.write(df[column_name].describe().to_frame().T)
+    # make a summary statistics table of the selected column in horizontal format and integer format
+    st.write(df[column_name].describe().to_frame().T)
 
-#     # if the user selects "Add New Data" then show the form
-#     case "Vendor Data":
-#         query_vendor_1 = "SELECT * FROM vendors_db"
-#         df_metric_1 = pd.read_sql(query_vendor_1, connection)
-#         query_vendor_2 = "SELECT month(transaction_date) as Month, count(distinct vendor_id) as Active_Vendors FROM transactions_log WHERE transaction_status = 'Borrowed' GROUP BY month(transaction_date)"
-#         df_metric_2 = pd.read_sql(query_vendor_2, connection)
+# if the user selects "Add New Data" then show the form
+elif selection == "Vendor Data":
+    query_vendor_1 = "SELECT * FROM vendors_db"
+    df_metric_1 = pd.read_sql(query_vendor_1, connection)
+    query_vendor_2 = "SELECT month(transaction_date) as Month, count(distinct vendor_id) as Active_Vendors FROM transactions_log WHERE transaction_status = 'Borrowed' GROUP BY month(transaction_date)"
+    df_metric_2 = pd.read_sql(query_vendor_2, connection)
 
-#         st.header("Vendor Data for 2022")
+    st.header("Vendor Data for 2022")
 
-#         # All the chart codes - not deployed yet
-#         stock_chart = (
-#             alt.Chart(df_metric_1)
-#             .mark_bar()
-#             .encode(
-#                 x=alt.X(
-#                     "vendor_name",
-#                     sort="-y",
-#                     axis=alt.Axis(labelAngle=-0),
-#                     title="Vendor Name",
-#                 ),
-#                 y=alt.Y("cup_stock", title="Cup Stock"),
-#                 color="vendor_name",
-#                 tooltip=[
-#                     alt.Tooltip("vendor_name", title="Vendor Name"),
-#                     alt.Tooltip("cup_stock", title="Cup Stock"),
-#                 ],
-#             )
-#             .interactive()
-#         )
+    # All the chart codes - not deployed yet
+    stock_chart = (
+        alt.Chart(df_metric_1)
+        .mark_bar()
+        .encode(
+            x=alt.X(
+                "vendor_name",
+                sort="-y",
+                axis=alt.Axis(labelAngle=-0),
+                title="Vendor Name",
+            ),
+            y=alt.Y("cup_stock", title="Cup Stock"),
+            color="vendor_name",
+            tooltip=[
+                alt.Tooltip("vendor_name", title="Vendor Name"),
+                alt.Tooltip("cup_stock", title="Cup Stock"),
+            ],
+        )
+        .interactive()
+    )
 
-#         # create an altair chart to show x:month(transaction_date), y count(distinct vendor_id) from df_metric_2
-#         vendor_chart = (
-#             alt.Chart(df_metric_2)
-#             .mark_bar()
-#             .encode(
-#                 x=alt.X("Month:N", title="Month",
-#                         axis=alt.Axis(labelAngle=-0)),
-#                 y=alt.Y("Active_Vendors:Q", title="Active Vendors"),
-#                 tooltip=[
-#                     alt.Tooltip("Month", title="Month"),
-#                     alt.Tooltip("Active_Vendors:Q", title="Active Vendors"),
-#                 ],
-#             )
-#             .interactive()
-#         )
+    # create an altair chart to show x:month(transaction_date), y count(distinct vendor_id) from df_metric_2
+    vendor_chart = (
+        alt.Chart(df_metric_2)
+        .mark_bar()
+        .encode(
+            x=alt.X("Month:N", title="Month",
+                    axis=alt.Axis(labelAngle=-0)),
+            y=alt.Y("Active_Vendors:Q", title="Active Vendors"),
+            tooltip=[
+                alt.Tooltip("Month", title="Month"),
+                alt.Tooltip("Active_Vendors:Q", title="Active Vendors"),
+            ],
+        )
+        .interactive()
+    )
 
-#         st.subheader("Cup Stock by Vendor")
-#         st.altair_chart(stock_chart, use_container_width=True)
+    st.subheader("Cup Stock by Vendor")
+    st.altair_chart(stock_chart, use_container_width=True)
 
-#         st.subheader("Active Vendors by Month")
-#         st.altair_chart(vendor_chart, use_container_width=True)
+    st.subheader("Active Vendors by Month")
+    st.altair_chart(vendor_chart, use_container_width=True)
 
-#     case "Customer Data":
-#         query_customer_1 = "SELECT month(join_date) as Month, COUNT(distinct customer_id) as new_user FROM customers_db GROUP BY month(join_date);"
-#         df_customer_1 = pd.read_sql(query_customer_1, connection)
-#         query_customer_2 = "SELECT month(transaction_date) as Month, count(distinct customer_id) as active_user FROM transactions_log WHERE transaction_status = 'Borrowed' GROUP BY month(transaction_date)"
-#         df_customer_2 = pd.read_sql(query_customer_2, connection)
+elif selection == "Customer Data":
+    query_customer_1 = "SELECT month(join_date) as Month, COUNT(distinct customer_id) as new_user FROM customers_db GROUP BY month(join_date);"
+    df_customer_1 = pd.read_sql(query_customer_1, connection)
+    query_customer_2 = "SELECT month(transaction_date) as Month, count(distinct customer_id) as active_user FROM transactions_log WHERE transaction_status = 'Borrowed' GROUP BY month(transaction_date)"
+    df_customer_2 = pd.read_sql(query_customer_2, connection)
 
-#         st.header("Customer Data for 2022")
-#         st.subheader("New Users by Month")
-#         # create an altair chart to show x:Month, y:new_user from df_metric_3
-#         customer_chart = (
-#             alt.Chart(df_customer_1)
-#             .mark_bar()
-#             .encode(
-#                 x=alt.X("Month:N", title="Month",
-#                         axis=alt.Axis(labelAngle=-0)),
-#                 y=alt.Y("new_user:Q", title="New Users"),
-#                 tooltip=[
-#                     alt.Tooltip("Month", title="Month"),
-#                     alt.Tooltip("new_user:Q", title="New Users"),
-#                 ],
-#             )
-#             .interactive()
-#         )
-#         st.altair_chart(customer_chart, use_container_width=True)
+    st.header("Customer Data for 2022")
+    st.subheader("New Users by Month")
+    # create an altair chart to show x:Month, y:new_user from df_metric_3
+    customer_chart = (
+        alt.Chart(df_customer_1)
+        .mark_bar()
+        .encode(
+            x=alt.X("Month:N", title="Month",
+                    axis=alt.Axis(labelAngle=-0)),
+            y=alt.Y("new_user:Q", title="New Users"),
+            tooltip=[
+                alt.Tooltip("Month", title="Month"),
+                alt.Tooltip("new_user:Q", title="New Users"),
+            ],
+        )
+        .interactive()
+    )
+    st.altair_chart(customer_chart, use_container_width=True)
 
-#         st.subheader("Active Users by Month")
-#         # create an altair line chart to show x:Month, y:Active_Users from df_metric_4
-#         customer_line_chart = (
-#             alt.Chart(df_customer_2)
-#             .mark_bar()
-#             .encode(
-#                 x=alt.X(
-#                     "Month:N",
-#                     title="Month",
-#                     axis=alt.Axis(labelAngle=-0),
-#                     scale=alt.Scale(zero=False),
-#                 ),
-#                 y=alt.Y("active_user:Q", title="Active Users",
-#                         scale=alt.Scale(zero=False)),
-#                 tooltip=[
-#                     alt.Tooltip("Month", title="Month"),
-#                     alt.Tooltip("active_user:Q", title="Active Users"),
-#                 ],
-#             )
-#             .interactive()
-#         )
-#         st.altair_chart(customer_line_chart, use_container_width=True)
-#         connection.close()
-#     case "Pull Customer Data":
-#         st.header("Pull User Data")
-#         st.write("Please enter the user ID below to pull the user data")
-#         # user_id = st.text_input("User ID", "")
-#         # if st.button("Pull Data"):
-#         #     query = "SELECT * FROM customers_db WHERE customer_id = " + user_id
-#         #     df = pd.read_sql(query, connection)
-#         #     st.write(df)
-#         # # give a choice of using user_name
-#         username = st.text_input("User Name", "")
-#         try:
-#             if st.button("Pull Data"):
-#                 query = (
-#                     "SELECT customer_id, customer_lastName, customer_firstName, join_date, cup_rental, deposit, cups_bought, account_value, user_name FROM customers_db WHERE user_name = "
-#                     + str(username)
-#                 )
-#                 df = pd.read_sql(query, connection)
-#                 st.write(df)
-#                 connection.close()
-#         except Exception as e:
-#             st.write("User not found")
-#             connection.close()
+    st.subheader("Active Users by Month")
+    # create an altair line chart to show x:Month, y:Active_Users from df_metric_4
+    customer_line_chart = (
+        alt.Chart(df_customer_2)
+        .mark_bar()
+        .encode(
+            x=alt.X(
+                "Month:N",
+                title="Month",
+                axis=alt.Axis(labelAngle=-0),
+                scale=alt.Scale(zero=False),
+            ),
+            y=alt.Y("active_user:Q", title="Active Users",
+                    scale=alt.Scale(zero=False)),
+            tooltip=[
+                alt.Tooltip("Month", title="Month"),
+                alt.Tooltip("active_user:Q", title="Active Users"),
+            ],
+        )
+        .interactive()
+    )
+    st.altair_chart(customer_line_chart, use_container_width=True)
+    connection.close()
+elif selection == "Pull Customer Data":
+    st.header("Pull User Data")
+    st.write("Please enter the user ID below to pull the user data")
+    # user_id = st.text_input("User ID", "")
+    # if st.button("Pull Data"):
+    #     query = "SELECT * FROM customers_db WHERE customer_id = " + user_id
+    #     df = pd.read_sql(query, connection)
+    #     st.write(df)
+    # # give a choice of using user_name
+    username = st.text_input("User Name", "")
+    try:
+        if st.button("Pull Data"):
+            query = (
+                "SELECT customer_id, customer_lastName, customer_firstName, join_date, cup_rental, deposit, cups_bought, account_value, user_name FROM customers_db WHERE user_name = "
+                + str(username)
+            )
+            df = pd.read_sql(query, connection)
+            st.write(df)
+            connection.close()
+    except Exception as e:
+        st.write("User not found")
+        connection.close()
 
-#     case "Add/Update Customer Data":
-#         # Add/Update Customer Data Page
-#         # Use a form to update customer data
-#         with st.form("Customer Sign-up"):
-#             # Collect customer data inputs
-#             st.header("Add New Customer Data or Update Existing Customer Data")
-#             st.write("Please enter the new customer data below")
-#             customer_id = st.text_input("Customer ID")
-#             customer_lastName = st.text_input("Customer Last Name")
-#             customer_firstName = st.text_input("Customer First Name")
-#             customer_join_date = st.date_input("Join Date", today)
-#             deposit = st.text_input("Deposit", 5)
-#             account_value = st.text_input("Account Value", 0)
-#             submitted = st.form_submit_button("Submit")
-#             # Submit inputs with form button
-#             if submitted:
-#                 # Consolidate user data
-#                 user = {
-#                     "customer_id": customer_id,
-#                     "customer_firstName": first_name,
-#                     "customer_lastName": last_name,
-#                     "join_date": date.today(),
-#                     "deposit": deposit,
-#                     "account_value": account_value
-#                 }
-#                 # Run query
-#                 try:
-#                     # check if customer_id already exists in customers_db
-#                     customerQuery = f"SELECT * FROM customers_db WHERE customer_id = {customer_id};"
-#                     customerResults = query(customerQuery)
-#                     # Add new user
-#                     if len(customerResults) == 0:
-#                         add_user(user)
-#                         st.write(
-#                             f"Congratulations {first_name} {last_name}!  You have signed-up for Cup Adventure!"
-#                         )
-#                         st.write(
-#                             "Thank you for joining us in reducing Cup Waste!")
-#                     # Update existing user
-#                     else:
-#                         update_user(user)
-#                         st.write(
-#                             f"{first_name} {last_name}'s information has been updated in the database.")
-#                 except:
-#                     st.write(
-#                         "There was an error signing you up.  Please ensure no fields are blank."
-#                     )
+elif selection == "Add/Update Customer Data":
+    # Add/Update Customer Data Page
+    # Use a form to update customer data
+    with st.form("Customer Sign-up"):
+        # Collect customer data inputs
+        st.header("Add New Customer Data or Update Existing Customer Data")
+        st.write("Please enter the new customer data below")
+        customer_id = st.text_input("Customer ID")
+        customer_lastName = st.text_input("Customer Last Name")
+        customer_firstName = st.text_input("Customer First Name")
+        customer_join_date = st.date_input("Join Date", today)
+        deposit = st.text_input("Deposit", 5)
+        account_value = st.text_input("Account Value", 0)
+        submitted = st.form_submit_button("Submit")
+        # Submit inputs with form button
+        if submitted:
+            # Consolidate user data
+            user = {
+                "customer_id": customer_id,
+                "customer_firstName": first_name,
+                "customer_lastName": last_name,
+                "join_date": date.today(),
+                "deposit": deposit,
+                "account_value": account_value
+            }
+            # Run query
+            try:
+                # check if customer_id already exists in customers_db
+                customerQuery = f"SELECT * FROM customers_db WHERE customer_id = {customer_id};"
+                customerResults = query(customerQuery)
+                # Add new user
+                if len(customerResults) == 0:
+                    add_user(user)
+                    st.write(
+                        f"Congratulations {first_name} {last_name}!  You have signed-up for Cup Adventure!"
+                    )
+                    st.write(
+                        "Thank you for joining us in reducing Cup Waste!")
+                # Update existing user
+                else:
+                    update_user(user)
+                    st.write(
+                        f"{first_name} {last_name}'s information has been updated in the database.")
+            except:
+                st.write(
+                    "There was an error signing you up.  Please ensure no fields are blank."
+                )
