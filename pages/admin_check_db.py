8f35ff847d6e2d65d2fd359bfd418b1180438da4
Wafiakmal <104295829+wafiakmal@users.noreply.github.com>
2022-12-15 14:44:43 +0000

    2

--- pages/admin_check_db.py
+++ pages/admin_check_db.py
@@ -48,8 +48,8 @@ st.write(df)
 # create a streamlit selectbox to select the column
 column_name = st.selectbox("Select a column", df.columns)
 
-# make a summary statistics table of the selected column
-st.write(df[column_name].describe())
+# make a summary statistics table of the selected column in horizontal format
+st.write(df[column_name].describe().to_frame().T)
 
 # close connection
 connection.close()
