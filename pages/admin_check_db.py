1993a26f28f0fec21080954f80e76562a74454a7
Wafiakmal <104295829+wafiakmal@users.noreply.github.com>
2022-12-15 14:30:34 +0000

    1

--- pages/admin_check_db.py
+++ pages/admin_check_db.py
@@ -1,48 +1,45 @@
-import mysql.connector
-from mysql.connector import Error
-import os
-import pandas as pd
-from sqlalchemy import create_engine
+# import mysql.connector
+# from mysql.connector import Error
+# import os
+# import pandas as pd
+# from sqlalchemy import create_engine
 import streamlit as st
 
 
-def create_db_connection(host_name, user_name, user_password, user_port, db_name):
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            port=user_port,
-            database=db_name,
-        )
-        print("MySQL Database connection successful")
-    except Error as err:
-        print(f"Error: '{err}'")
+# def create_db_connection(host_name, user_name, user_password, user_port, db_name):
+#     connection = None
+#     try:
+#         connection = mysql.connector.connect(
+#             host=host_name,
+#             user=user_name,
+#             passwd=user_password,
+#             port=user_port,
+#             database=db_name,
+#         )
+#         print("MySQL Database connection successful")
+#     except Error as err:
+#         print(f"Error: '{err}'")
 
-    return connection
+#     return connection
 
 
-connection = create_db_connection(
-    os.getenv("AWS_HOST"),
-    os.getenv("AWS_USER"),
-    os.getenv("AWS_PASSWORD"),
-    os.getenv("AWS_PORT"),
-    "cup_adventure",
-)
+# connection = create_db_connection(
+#     os.getenv("AWS_HOST"),
+#     os.getenv("AWS_USER"),
+#     os.getenv("AWS_PASSWORD"),
+#     os.getenv("AWS_PORT"),
+#     "cup_adventure",
+# )
 
-# call all table names from cup_adventure database
-cursor = connection.cursor()
-cursor.execute("SHOW TABLES")
-tables = cursor.fetchall()
+# # make a list of all the table in cup_adventure database
+# tables = ["cups_db", "customers_db", "transactions", "transactions_log", "vendors_db"]
 
-# make a list of all the table in cup_adventure database
-tables = ["cups_db", "customers_db", "transactions", "transactions_log", "vendors_db"]
+# # create a streamlit selectbox to select the table
+# table_name = st.selectbox("Select a table", tables)
 
-# create a streamlit selectbox to select the table
-table_name = st.selectbox("Select a table", tables)
+# # query the database based on table_name selected and preserve the column names
+# query = "SELECT * FROM " + table_name
+# df = pd.read_sql(query, connection)
+# st.write(df)
 
-# query the database based on table_name selected and preserve the column names
-query = "SELECT * FROM " + table_name
-df = pd.read_sql(query, connection)
-st.write(df)
+st.write("Hello World")
