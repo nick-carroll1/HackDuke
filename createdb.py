47d4a867d36bcd9eb5089b7474dbc83d45f8e7ba
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-12-16 04:39:14 +0000

    Updated returns functionality

--- createdb.py
+++ createdb.py
@@ -339,7 +339,7 @@ if __name__ == "__main__":
     # myquery = "INSERT INTO customers (customer_firstName, customer_lastName, join_date) VALUES ('Jenny', 'Shen', '2022-12-02');"
     # myquery = "SELECT password FROM customers_db where user_name = 'ngift1';"
     # myquery = f"SELECT DISTINCT vendor_id, vendor_name FROM vendors_db;"
-    myquery = f"SELECT * FROM transactions_log;"
+    myquery = f"SELECT * FROM customers_db;"
     # myquery = f"SELECT MAX(order_id) + 1 FROM transactions_log;"
     # myquery = f"SELECT customer_id FROM customers_db WHERE user_name = '{user}';"
     # myquery = "SHOW TABLES;"
