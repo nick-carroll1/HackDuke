2f7ddf14ee8ab7de7ea0306fc7ee0eb7392b7017
Nick Carroll <112578073+nick-carroll1@users.noreply.github.com>
2022-12-16 05:25:51 +0000

    Consolidated Analytics Functionality

--- pages/Administrator_Functions.py
+++ pages/Administrator_Functions.py
@@ -11,8 +11,6 @@ selection = st.sidebar.radio(
     [
         "Welcome Page",
         "Read All Data",
-        "Vendor Data",
-        "Customer Data",
         "Pull Customer Data",
         "Add/Update Customer Data",
         "Transactions"
@@ -88,106 +86,7 @@ if check_password():
 
         # make a summary statistics table of the selected column in horizontal format and integer format
         st.table(df[column_name].describe().to_frame().T)
-
-    # if the user selects "Add New Data" then show the form
-    elif selection == "Vendor Data":
-        query_vendor_1 = "SELECT * FROM vendors_db"
-        df_metric_1 = querydf(query_vendor_1)
-        query_vendor_2 = "SELECT month(transaction_date) as Month, count(distinct vendor_id) as Active_Vendors FROM transactions_log WHERE transaction_status = 'Borrowed' GROUP BY month(transaction_date)"
-        df_metric_2 = querydf(query_vendor_2)
-
-        st.header("Vendor Data for 2022")
-
-        # All the chart codes - not deployed yet
-        stock_chart = (
-            alt.Chart(df_metric_1)
-            .mark_bar()
-            .encode(
-                x=alt.X(
-                    "vendor_name",
-                    sort="-y",
-                    axis=alt.Axis(labelAngle=-0),
-                    title="Vendor Name",
-                ),
-                y=alt.Y("cup_stock", title="Cup Stock"),
-                color="vendor_name",
-                tooltip=[
-                    alt.Tooltip("vendor_name", title="Vendor Name"),
-                    alt.Tooltip("cup_stock", title="Cup Stock"),
-                ],
-            )
-            .interactive()
-        )
-
-        # create an altair chart to show x:month(transaction_date), y count(distinct vendor_id) from df_metric_2
-        vendor_chart = (
-            alt.Chart(df_metric_2)
-            .mark_bar()
-            .encode(
-                x=alt.X("Month:N", title="Month",
-                        axis=alt.Axis(labelAngle=-0)),
-                y=alt.Y("Active_Vendors:Q", title="Active Vendors"),
-                tooltip=[
-                    alt.Tooltip("Month", title="Month"),
-                    alt.Tooltip("Active_Vendors:Q", title="Active Vendors"),
-                ],
-            )
-            .interactive()
-        )
-
-        st.subheader("Cup Stock by Vendor")
-        st.altair_chart(stock_chart, use_container_width=True)
-
-        st.subheader("Active Vendors by Month")
-        st.altair_chart(vendor_chart, use_container_width=True)
-
-    elif selection == "Customer Data":
-        query_customer_1 = "SELECT month(join_date) as Month, COUNT(distinct customer_id) as new_user FROM customers_db GROUP BY month(join_date);"
-        df_customer_1 = querydf(query_customer_1)
-        query_customer_2 = "SELECT month(transaction_date) as Month, count(distinct customer_id) as active_user FROM transactions_log WHERE transaction_status = 'Borrowed' GROUP BY month(transaction_date)"
-        df_customer_2 = querydf(query_customer_2)
-
-        st.header("Customer Data for 2022")
-        st.subheader("New Users by Month")
-        # create an altair chart to show x:Month, y:new_user from df_metric_3
-        customer_chart = (
-            alt.Chart(df_customer_1)
-            .mark_bar()
-            .encode(
-                x=alt.X("Month:N", title="Month",
-                        axis=alt.Axis(labelAngle=-0)),
-                y=alt.Y("new_user:Q", title="New Users"),
-                tooltip=[
-                    alt.Tooltip("Month", title="Month"),
-                    alt.Tooltip("new_user:Q", title="New Users"),
-                ],
-            )
-            .interactive()
-        )
-        st.altair_chart(customer_chart, use_container_width=True)
-
-        st.subheader("Active Users by Month")
-        # create an altair line chart to show x:Month, y:Active_Users from df_metric_4
-        customer_line_chart = (
-            alt.Chart(df_customer_2)
-            .mark_bar()
-            .encode(
-                x=alt.X(
-                    "Month:N",
-                    title="Month",
-                    axis=alt.Axis(labelAngle=-0),
-                    scale=alt.Scale(zero=False),
-                ),
-                y=alt.Y("active_user:Q", title="Active Users",
-                        scale=alt.Scale(zero=False)),
-                tooltip=[
-                    alt.Tooltip("Month", title="Month"),
-                    alt.Tooltip("active_user:Q", title="Active Users"),
-                ],
-            )
-            .interactive()
-        )
-        st.altair_chart(customer_line_chart, use_container_width=True)
+    # User selects Pull Customer Data
     elif selection == "Pull Customer Data":
         st.header("Pull User Data")
         st.write("Please enter the Customer ID below to pull the user data")
